8c37581b72165e0e7e1e5f04a2708c65312a399e
William Walter <Bushfries@gmail.com>
2020-05-22 02:02:32 -0500

    Removed the good stuff

--- CoolBot/APIAccess.py
+++ CoolBot/APIAccess.py
@@ -2,10 +2,10 @@ import tweepy
 
 # --------------------------------------------------------BUSHFRIES----------------------------------------------------
 try:
-    get_consumer_key = 'lZKHiGxzaac9eApuedZ8FuBlR'
-    get_consumer_secret = 'QBo2RJob1Jz9QUg67S47cGPg3enQRwnQHii1r9Al1ikDxcQLG5'
-    get_access_token = '834452681355882496-aCwWCJ3ynIP15j5afgM9e5KrPKXUjQp'
-    get_access_token_secret = 'VTe2Hd9Rki4JxBJYk9siJf7hxXmiAPpn8FKCHtJh6oFss'
+    get_consumer_key = ''
+    get_consumer_secret = ''
+    get_access_token = ''
+    get_access_token_secret = ''
 
     get_auth = tweepy.OAuthHandler(get_consumer_key,
                                    get_consumer_secret)
@@ -17,10 +17,10 @@ except tweepy.error.TweepError:
 
 # --------------------------------------------------------BOTFRIES----------------------------------------------------
 try:
-    post_consumer_key = 'drwRd4caSmINAvTC1A7tx7xA2'
-    post_consumer_secret = 'XPVTGkclLB4HeXEQx9pfbg3twWlMfthj5EDaLlElJUMvvTclk9'
-    post_access_token = '974810262401363968-GddtODwIvoytzgTj3gZmDIk6QK3aBB4'
-    post_access_token_secret = 'awqFqQ1o7XPuKfHpyUrLEOBgMBDfB7sAtvzhzYenCyUWW'
+    post_consumer_key = ''
+    post_consumer_secret = ''
+    post_access_token = ''
+    post_access_token_secret = ''
 
     post_auth = tweepy.OAuthHandler(post_consumer_key,
                                     post_consumer_secret)
